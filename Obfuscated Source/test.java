bd65c9edda6a141f5c443b7d2afc2d37b0151314
Muzamil Khan <mkha537@aucklanduni.ac.nz>
2017-04-26 00:18:59 +1200

    added opaque predictes

--- Obfuscated Source/test.java
+++ Obfuscated Source/test.java
@@ -6,28 +6,84 @@ package obfuscator;
 public class test {
 
     public void TestMethod() {
-        this.processData();
-        this.computeService();
+        int cashFlowThreshold = 4, spendingTurnover = 10;
+        double avgTurnoverRatio = Math.random()*20;
+        if (avgTurnoverRatio > spendingTurnover)
+            return true;
+        else
+            return false;
+        if (spendingTurnover > (Math.pow(cashFlowThreshold, 3) * avgTurnoverRatio))
+            return true;
+        else
+            return false;
+        if (spendingTurnover % 3 != 0)
+            return true;
+        else
+            return false;
         this.checkPrimaryCondition(5);
+        this.computeService();
+        this.processData();
         System.out.println("testing");
         System.out.println("testing");
         System.out.println("testing");
     }
 
     public void AnotherTestMethod() {
-        int a = 10;
+        int cashFlowThreshold = 4, spendingTurnover = 10;
+        double avgTurnoverRatio = Math.random()*20;
+        if (avgTurnoverRatio > spendingTurnover)
+            return true;
+        else
+            return false;
+        if (spendingTurnover > (Math.pow(cashFlowThreshold, 3) * avgTurnoverRatio))
+            return true;
+        else
+            return false;
+        if (spendingTurnover % 3 != 0)
+            return true;
+        else
+            return false;
         this.computeService();
-        int b = 5;
+        int a = 10;
         this.checkPrimaryCondition(5);
+        int b = 5;
         this.processData();
         System.out.println(a + b);
     }
 
     public int processData() {
+        int cashFlowThreshold = 4, spendingTurnover = 10;
+        double avgTurnoverRatio = Math.random()*20;
+        if (avgTurnoverRatio > spendingTurnover)
+            return true;
+        else
+            return false;
+        if (spendingTurnover > (Math.pow(cashFlowThreshold, 3) * avgTurnoverRatio))
+            return true;
+        else
+            return false;
+        if (spendingTurnover % 3 != 0)
+            return true;
+        else
+            return false;
         return 5 * 5;
     }
 
     public boolean checkPrimaryCondition(int a) {
+        int cashFlowThreshold = 4, spendingTurnover = 10;
+        double avgTurnoverRatio = Math.random()*20;
+        if (avgTurnoverRatio > spendingTurnover)
+            return true;
+        else
+            return false;
+        if (spendingTurnover > (Math.pow(cashFlowThreshold, 3) * avgTurnoverRatio))
+            return true;
+        else
+            return false;
+        if (spendingTurnover % 3 != 0)
+            return true;
+        else
+            return false;
         if (a == 0)
             return true;
         else
@@ -35,6 +91,20 @@ public class test {
     }
 
     public void computeService() {
+        int cashFlowThreshold = 4, spendingTurnover = 10;
+        double avgTurnoverRatio = Math.random()*20;
+        if (avgTurnoverRatio > spendingTurnover)
+            return true;
+        else
+            return false;
+        if (spendingTurnover > (Math.pow(cashFlowThreshold, 3) * avgTurnoverRatio))
+            return true;
+        else
+            return false;
+        if (spendingTurnover % 3 != 0)
+            return true;
+        else
+            return false;
         int a = 1;
         int b = 0;
         a == b ? this.processData() : this.checkPrimaryCondition(2);
